e9e64b4165dbb0fd0fa82d543aec41b33d21f346
201210039 <201210039@SKMNS201210039>
2018-08-26 16:09:29 +0900

    상품카테고리 메인 배너 개발

--- ligthbank/src/skin2/js/module/product/product_selectsearch.js
+++ ligthbank/src/skin2/js/module/product/product_selectsearch.js
@@ -3,9 +3,57 @@
  */
 $(document).ready(function()
 {
-    //배너 변경 
-    $("#subMainBanner").text($("#titleNmUseBanner").text()+" 배너공간");
-  
+    //배너 변경
+	if($(".banner").html() == "" && $("#loc_1depth").text().indexOf("135") < 0){
+		$("#subMainBanner").show();
+		  //우측슬라이드배너
+	    $('.JS_23165_01').DB_tabArrowSlideMove({
+	        motionType:'x',          //모션타입('none','x','y','fade')
+	        motionSpeed:800,         //모션속도
+	        autoRollingTime:3000,    //자동롤링속도(밀리초)
+	        arrowVisible:true,       //메뉴보이기(true,false)
+	        overRolling:false        //마우스오버시작동(true,false)
+	    });
+	    		
+		//거실, 안방 
+		if($("#loc_1depth").text().indexOf("123") > 0 || $("#loc_1depth").text().indexOf("124") > 0){
+			$("#banner1").html("<a href='/product/list.html?cate_no=100'><img id='bannerType1' src='/_wg/img/sub_banner/sbt_03_01.jpg' /></a>");
+			$("#banner2").hide();
+			$("#bannerTab").show();
+		}else{
+			//공부방/아이방
+			if($("#loc_1depth").text().indexOf("125") > 0 || $("#loc_1depth").text().indexOf("126") > 0){
+				$("#banner1").html("<img src='/_wg/img/sub_banner/sb_03.jpg' />");	
+			//주방			
+			}else if($("#loc_1depth").text().indexOf("130") > 0){ 
+				$("#banner1").html("<img src='/_wg/img/sub_banner/sb_04.jpg' />");	
+			//알파/드레스룸			
+			}else if($("#loc_1depth").text().indexOf("127") > 0){ 
+				$("#banner1").html("<img src='/_wg/img/sub_banner/sb_05.jpg' />");	
+			//욕실		
+			}else if($("#loc_1depth").text().indexOf("132") > 0){ 
+				$("#banner1").html("<img src='/_wg/img/sub_banner/sb_06.jpg' />");
+			//식탁/카페		
+			}else if($("#loc_1depth").text().indexOf("131") > 0){ 
+				$("#banner1").html("<img src='/_wg/img/sub_banner/sb_07.jpg' />");
+			//식탁/카페		
+			}else if($("#loc_1depth").text().indexOf("131") > 0){ 
+				$("#banner1").html("<img src='/_wg/img/sub_banner/sb_07.jpg' />");
+			//LED모듈		
+			}else if($("#loc_1depth").text().indexOf("134") > 0){ 
+				$("#banner1").html("<img src='/_wg/img/sub_banner/sb_08.jpg' />");
+			//현관/베란다/벽		
+			}else if($("#loc_1depth").text().indexOf("133") > 0){ 
+				$("#banner1").html("<img src='/_wg/img/sub_banner/sb_09.jpg' />");
+			}
+			
+			$("#banner2").hide();
+			$("#bannerTab").hide();
+		}
+	}else{
+		$("#subMainBanner").hide();
+	}
+	
     var sParentElement = 'select.SecondSelect';
     $('select.FirstSelect').change(function()
     {
@@ -54,3 +102,17 @@ $(document).ready(function()
         return $("<option value='" + sVal + "'>" + sTitle + "</option>");
     }
 });
+
+function chgBanner(type){
+	if(type == 1){
+		$("#samsungBtn").attr("src","/_wg/img/sub_banner/sb_tab01_on.jpg");
+		$("#setBtn").attr("src","/_wg/img/sub_banner/sb_tab02_off.jpg");
+		$("#banner1").show();
+		$("#banner2").hide();
+	}else{
+		$("#samsungBtn").attr("src","/_wg/img/sub_banner/sb_tab01_off.jpg");
+		$("#setBtn").attr("src","/_wg/img/sub_banner/sb_tab02_on.jpg");
+		$("#banner1").hide();
+		$("#banner2").show();
+	}
+}
