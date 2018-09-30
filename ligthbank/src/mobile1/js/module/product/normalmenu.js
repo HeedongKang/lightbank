c2836b8f3e6fa289edd2d202f2f147bc49b1750e
newpioneer <newpioneer@DESKTOP-OS10E0K>
2018-09-30 23:37:29 +0900

    상품리스트 개편

--- ligthbank/src/mobile1/js/module/product/normalmenu.js
+++ ligthbank/src/mobile1/js/module/product/normalmenu.js
@@ -8,6 +8,8 @@ var sQueryString = aUrl[1];
  * 파라미터가 있을경우에만 처리
  */
 $(document).ready(function(){
+	$(".newPrdList").hide();
+	
     if (sQueryString && sQueryString.indexOf('sort_method') > -1) {
         for (var i=0; i<$('#selArray option').length; i++) {
             if ($('#selArray option:eq('+i+')').val().indexOf(sQueryString) > -1) {
@@ -25,4 +27,17 @@ $('#selArray').change(function() {
 
 function goThumg(url) {
     location.href = url+'?'+sQueryString;
+}
+function chgRecommendTab(type){
+	if(type == 'BEST'){
+		$("#bestBtn").attr("src","/_wg/img/tab_title_best_on.jpg");
+		$("#newBtn").attr("src","/_wg/img/tab_title_new_off.png");
+		$(".bestPrdList").show();
+		$(".newPrdList").hide();
+	}else{
+		$("#bestBtn").attr("src","/_wg/img/tab_title_best_off.jpg");
+		$("#newBtn").attr("src","/_wg/img/tab_title_new_on.png");
+		$(".bestPrdList").hide();
+		$(".newPrdList").show();
+	}
 }
--- ligthbank/src/skin2/js/module/product/product_selectsearch.js
+++ ligthbank/src/skin2/js/module/product/product_selectsearch.js
@@ -120,8 +120,12 @@ function chgRecommendTab(type){
 	if(type == 'BEST'){
 		$("#bestBtn").attr("src","/_wg/img/tab_title_best_on.jpg");
 		$("#newBtn").attr("src","/_wg/img/tab_title_new_off.png");
+		$("#bestPrdList").show();
+		$("#newPrdList").hide();
 	}else{
 		$("#bestBtn").attr("src","/_wg/img/tab_title_best_off.jpg");
 		$("#newBtn").attr("src","/_wg/img/tab_title_new_on.png");
+		$("#bestPrdList").hide();
+		$("#newPrdList").show();
 	}
 }
